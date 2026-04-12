9492d002a9f4608a9c2cb914592566073703054a
Louis Gandara <crazy@LAPTOP-ULL8SV7B>
2026-04-12 17:23:29 -0600

    2

---
+++ 2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Desarrolle un programa que solicite el precio de un producto, 
+ * validando que sea un número decimal mayor que 0, repitiendo hasta que el usuario ingrese correctamente el valor
+ */
+double price;
+bool correct;
+
+do
+{
+    Console.WriteLine("Ingrese precio: ");
+    correct = double.TryParse(Console.ReadLine(), out price);
+    if (!correct || price <= 0)
+    {
+        Console.WriteLine("Precio inválido");
+        correct = false;
+        Console.ReadKey();
+    }
+} while (!correct);
+
+Console.WriteLine($"El precio es: Q{price}");
