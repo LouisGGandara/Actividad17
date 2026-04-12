d09d4ef21a3a30ef7b5b6c728faa4ae9e4916c8c
Louis Gandara <crazy@LAPTOP-ULL8SV7B>
2026-04-12 16:59:18 -0600

    1

---
+++ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Desarrolle un programa que solicite la edad de una persona, validando que el valor ingresado 
+ * sea un número entero válido, repitiendo hasta que el dato sea correcto y mostrando la edad ingresada
+ * */
+int age;
+bool correct;
+
+do
+{
+    Console.WriteLine("Ingrese edad: ");
+    correct = int.TryParse(Console.ReadLine(), out age);
+    if (!correct || (age <0 || age > 120))
+    {
+        Console.WriteLine("Dato inválido");
+        correct = false;
+        Console.ReadKey();
+    }
+} while (!correct);
+
+Console.WriteLine($"Edad es: {age}");
